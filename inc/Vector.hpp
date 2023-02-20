7173dd4e7d5edb4a57ee60111b82462482cb7e0b
aposipov <a.p.osipov@ro.ru>
2023-02-20 17:24:04 +1000

    vector

--- inc/Vector.hpp
+++ inc/Vector.hpp
@@ -3,16 +3,122 @@
 #define FT_CONTAINERS_VECTOR_HPP
 
 #include <iostream>
+//#include
+//#include "iterator.h"
 
 namespace ft {
 
 	template < class T, class Allocator = std::allocator<T> >
-	class vector {
+	class vector
+	{
 
 	public:
 
+		/* ------------------------- member types ------------------------- */
+		typedef T value_type;
+		typedef Allocator allocator_type;
+		typedef std::size_t size_type;
+		typedef std::ptrdiff_t difference_type;
+		typedef value_type &reference;
+		typedef const value_type &const_reference;
+		typedef typename Allocator::pointer pointer;
+		typedef typename Allocator::const_pointer const_pointer;
 
+		/* ------------------------- iterators ------------------------- */
+//		typedef random_access_it<T>						iterator;
+//		typedef	random_access_it<const T>				const_iterator;
+//		typedef reverse_it<iterator>					reverse_iterator;
+//		typedef reverse_it<const_iterator>				const_reverse_iterator;
+
+		/* ------------------------- constructors ------------------------- */
+		explicit vector(const allocator_type &alloc = Allocator()) : _alloc(alloc), _size(0), _capacity(0), _array(NULL)
+		{}
+
+		// try ?
+		explicit vector(size_type n, const T &val = T(), const Allocator &alloc = Allocator()) :
+				_alloc(alloc), _size(n), _capacity(n), _array(_alloc.allocate(n))
+		{
+			for (size_type i = 0; i < _size; i++)
+				_alloc.construct(_array + i, val);
+		}
+
+		template<class InputIt>
+		vector(InputIt first, InputIt last, const Allocator &alloc = Allocator())
+		{}
+
+		vector(const vector &other)
+		{}
+
+		/* ------------------------- destructors ------------------------- */
+		~vector()
+		{}
+
+		/* ------------------------- member functions ------------------------- */
+		vector &operator=(const vector &other)
+		{}
+//		assign
+
+//		get_allocator
+		allocator_type get_allcator() const
+		{
+			return _alloc;
+		}
+
+		/* ------------------------- element access ------------------------- */
+//		at
+		reference at(size_type pos)
+		{
+			if (!(pos < _size))
+				throw std::out_of_range("Error: Vector at");
+			return *(_array + pos);
+		}
+
+		const_reference at(size_type pos) const
+		{
+			if (!(pos < _size))
+				throw std::out_of_range("Error: Vector at");
+			return *(_array + pos);
+		}
+
+//		operator[]
+		reference operator[](size_type pos) { return *(_array + pos); }
+		const_reference operator[](size_type pos) const { return *(_array + pos); }
+
+//		front
+		reference front() { return _array[0]; }
+		const_reference front() const { return _array[0]; }
+
+//		back
+		reference back() { return (_size - 1); }
+		const_reference back() const { return (_size - 1); }
+
+//		data
+		pointer data() { return _array; }
+		const_pointer data() const { return _array; }
+
+		/* ------------------------- iterators m ------------------------- */
+//		iterators
+//		rever iterators
+
+		/* ------------------------- capacity ------------------------- */
+//		empty
+//		size
+//		max_size
+//		reserve
+//		capacity
+
+		/* ------------------------- modifiers ------------------------- */
+
+
+		/* ------------------------- non-member functions ------------------------- */
+
+	private:
+		allocator_type _alloc;
+		size_type _size;
+		size_type _capacity;
+		pointer _array;
 	};
+
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,38 @@
 
+#include <iostream>
+#include <vector>
+
 #include "Vector.hpp"
 
+int main()
+{
+	std::vector<int> v;
+	std::vector<int> second(4, 100);
+	std::allocator<int> alloc;
+	ft::vector<int> myv(4);
+
+//	for (int i = 0; i < 10; i++)
+//	{
+//		v.push_back(i);
+//		std::cout << "[" << i + 10 << "] = " << v[i + 10] << std::endl;
+//		std::cout << "size = " << v.size() << std::endl;
+//		std::cout << "capacity = " << v.capacity() << std::endl;
+//	}
+
+	second.at(2) = 33;
+	second[3] = 21;
+//	std::cout << "capacity = " << v.get_allocator() << std::endl;
+	std::cout << "size = " << second.size() << std::endl;
+	std::cout << "capacity = " << second.capacity() << std::endl;
+	std::cout << "pointer = " << second.data() << std::endl;
+	std::cout << "at = " << second.at(1) << std::endl;
+	std::cout << "[]  = " << second[3] << std::endl;
+//	std::allocator<int>::pointer
+
+	myv[1] = 42;
+	std::cout << "*[] = " << myv[1] << std::endl;
+	std::cout << "*at = " << myv.at(10) << std::endl;
+	std::cout << "front = " << myv.front() << std::endl;
+
+	return 0;
+}

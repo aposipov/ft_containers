c1643d41551a51b0aabfcc866dd85c01356aa5be
aposipov <a.p.osipov@ro.ru>
2023-03-09 23:30:56 +1000

    map begin

--- inc/Map.hpp
+++ inc/Map.hpp
@@ -5,4 +5,10 @@
 #ifndef FT_CONTAINERS_MAP_HPP
 #define FT_CONTAINERS_MAP_HPP
 
+#include <iostream>
+
+template< class Key, class T, class Compare = less<Key>,
+		class Allocator = std::allocator<pair<const Key, T> > >
+class map{};
+
 #endif //FT_CONTAINERS_MAP_HPP

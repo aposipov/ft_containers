a49d034095a46fd327280a570e3ce63d6f5fb2b5
aposipov <a.p.osipov@ro.ru>
2023-03-13 23:30:29 +1000

    map all declaration

--- inc/Map.hpp
+++ inc/Map.hpp
@@ -31,10 +31,13 @@ namespace ft {
 			typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
 
 			class value_compare {
+
 				friend class map;
+
 			protected:
 				Compare comp;
 				value_compare(Compare c) : comp(c) {}
+
 			public:
 				bool operator()(const value_type& x, const value_type& y) const
 				{
@@ -51,12 +54,114 @@ namespace ft {
 			map(InputIt first, InputIt last, const Compare& comp = Compare(), const Allocator& alloc = Allocator()) {}
 
 			map(const map& x) {}
+
 			/* ------------------------- destructor ------------------------- */
 
 			~map();
 
+			/* ------------------------- member functions ------------------------- */
+			map& operator=(const map& other);
+
+			allocator_type get_allocator() const;
+
+			/* ------------------------- element access ------------------------- */
+			T& at( const Key& key );
+
+			const T& at( const Key& key ) const;
+
+			T& operator[]( const Key& key );
+
+			/* ------------------------- iterators ------------------------- */
+			iterator begin();
+
+			const_iterator begin() const;
+
+			iterator end();
+
+			const_iterator end() const;
+
+			reverse_iterator rbegin();
+
+			const_reverse_iterator rbegin() const;
+
+			reverse_iterator rend();
+
+			const_reverse_iterator rend() const;
+
+			/* ------------------------- capacity ------------------------- */
+			bool empty() const;
+
+			size_type size() const;
+
+			size_type max_size() const;
+
+			/* ------------------------- modifiers ------------------------- */
+			void clear();
+
+			ft::pair<iterator, bool> insert( const value_type& value );
+
+			iterator insert( iterator pos, const value_type& value );
+
+			iterator erase( iterator pos );
+
+			iterator erase( iterator first, iterator last );
+
+//			size_type erase( const Key& key );
+
+			void swap( map& other );
+
+			/* ------------------------- lookup ------------------------- */
+			size_type count( const Key& key ) const;
+
+			iterator find( const Key& key );
+
+			const_iterator find( const Key& key ) const;
+
+			ft::pair<iterator,iterator> equal_range( const Key& key );
+
+			ft::pair<const_iterator,const_iterator> equal_range( const Key& key ) const;
+
+			iterator lower_bound( const Key& key );
+
+			const_iterator lower_bound( const Key& key ) const;
+
+			iterator upper_bound( const Key& key );
+
+			const_iterator upper_bound( const Key& key ) const;
+
+			/* ------------------------- observers ------------------------- */
+			key_compare key_comp() const;
+
+			ft::map::value_compare value_comp() const;
 
 	};
+
+	/* ------------------------- non-member functions ------------------------- */
+
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator==( const std::map<Key, T, Compare, Alloc>& lhs,
+					 const std::map<Key, T, Compare, Alloc>& rhs );
+
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator!=( const std::map<Key, T, Compare, Alloc>& lhs,
+					 const std::map<Key, T, Compare, Alloc>& rhs );
+
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator<( const std::map<Key, T, Compare, Alloc>& lhs,
+					const std::map<Key, T, Compare, Alloc>& rhs );
+
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator<=( const std::map<Key, T, Compare, Alloc>& lhs,
+					 const std::map<Key, T, Compare, Alloc>& rhs );
+
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator>( const std::map<Key, T, Compare, Alloc>& lhs,
+					const std::map<Key, T, Compare, Alloc>& rhs );
+
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator>=( const std::map<Key, T, Compare, Alloc>& lhs,
+					 const std::map<Key, T, Compare, Alloc>& rhs );
+
 }
 
 #endif
